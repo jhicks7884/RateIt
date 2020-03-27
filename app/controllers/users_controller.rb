581376a5f75e404ad67234243bed0a34937caa60
jeshua hicks <56523111+jhicks7884@users.noreply.github.com>
2020-03-27 18:03:01 -0400

    followed convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
         if @user.save
           session[:user_id] = @user.id
-          redirect_to '/show'
+          redirect_to '/users/:id'
         else
           redirect_to '/login'
         end

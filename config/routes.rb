7008a23a09805ca1b149d5237512ee3b677d028a
jeshua hicks <56523111+jhicks7884@users.noreply.github.com>
2020-03-27 17:57:52 -0400

    made routes more restful

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
        #users actions
+       # implement resources but get an understanding first.
 
   get 'home', to: 'users#home'
 
@@ -10,20 +11,20 @@ Rails.application.routes.draw do
   get 'login', to: 'users#login'
   post '/login', to: 'sessions#logged_in'
 
-  get '/show', to: 'users#show'
+  get '/users/:id', to: 'users#show'
   get '/logout', to: 'users#logout'
 
       #Vehicle action
 
    get "/vehicles/new", to: 'vehicles#new'
-   get "/vehicles/selections", to: 'vehicles#show'
+   get "/vehicles/:id", to: 'vehicles#show'
    post "/vehicles/selections", to: 'vehicles#create_vehicles'
    get "/vehicles/index", to: 'vehicles#index'
 
    #Vehicle Ratings
 
    get "/ratings/new", to: 'ratings#new'
-   #get "/ratings/", to: 'ratings#show'
+   #get "/ratings/:id", to: 'ratings#show'
    post "/ratings/new", to: 'ratings#create'
 
 

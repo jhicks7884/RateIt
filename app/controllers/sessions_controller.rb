83860c22f928a9703b2a047d2e7fc3ffc3234d07
jeshua hicks <56523111+jhicks7884@users.noreply.github.com>
2020-03-27 22:59:15 -0400

    fixed url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:user][:username])
     if @user.try(:authenticate, params[:user][:password])
          session[:user_id] = @user.id
-       redirect_to '/show'
+       redirect_to '/users/:id'
 
     else
         redirect_to '/login'
